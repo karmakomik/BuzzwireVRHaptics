2e865189109ec1c705ca4d75b5deb52a91a1c01b
Unnikrishnan Radhakrishnan <au657021@uni.au.dk>
2022-05-10 21:10:22 +0200

    Added Pilot test phase 1's vibration perception test and tests for equalizing stiffness and vibration.

--- Assets/PilotTestControllerScript.cs
+++ Assets/PilotTestControllerScript.cs
@@ -4,41 +4,85 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
-
+using Unity.Mathematics;
 public class PilotTestControllerScript : MonoBehaviour
 {
-    StreamWriter sw;
+    StreamWriter sw1, sw2;
+
+    public HapticPlugin HapticDevice = null;
 
     public float vibrationDuration = 3f;
 
     public List<GameObject> participantChoiceDropDownLists;
+    public GameObject tutorialControlUI;
     public GameObject pilotTest1ControlUI;
     public GameObject pilotTest2ControlUI;
 
     public Slider vibrationSlider;
     public Slider stiffnessSlider;
+    public Slider dampingSlider;
+    public Slider staticFrictionSlider;
+    public Slider dynamicFrictionSlider;
+
+    float vibrationSliderVal;
+    float stiffnessSliderVal;
+    float dampingSliderVal;
+    float staticFrictionSliderVal;
+    float dynamicFrictionSliderVal;
+
+    public GameObject groundedSurfaceObj;
+    public GameObject vibrationSurfaceObj;
+
     public TMPro.TextMeshProUGUI vibrationSliderValueText;
     public TMPro.TextMeshProUGUI stiffnessSliderValueText;
+    public TMPro.TextMeshProUGUI dampingSliderValueText;
+    public TMPro.TextMeshProUGUI staticFrictionSliderValueText;
+    public TMPro.TextMeshProUGUI dynamicFrictionSliderValueText;
 
     public GameObject currSurface;
     public GameObject currSurfaceUIText;
     int currTrialNum;
     int currMotorSeqInd;
+    int currStiffnessInd;
     int currTrialSurfaceIndex;
     public GameObject currTrialNumUIText;
+    public GameObject currVibrationTrialNumUIText;
+    public GameObject currStiffnessLevelUIText;
 
-
-    public string[,] motorSequences = new string[5,6] {
+    public string[,] motorSequences = new string[5, 6] {
                                 { "a", "f", "b", "d", "c", "e" },
                                 { "b", "a", "f", "e", "d", "c" },
                                 { "c", "b", "a", "f", "e", "d" },
                                 { "e", "c", "b", "a", "f", "d" },
-                                { "f", "d", "c", "b", "a", "e" }};      
+                                { "f", "d", "c", "b", "a", "e" }};
 
-    public List<GameObject> trial2Surfaces;
-    public List<GameObject> trial3Surfaces;
-    public List<GameObject> trial4Surfaces;
-    public List<GameObject> trial5Surfaces;
+    public string[,] stiffnessSequences = new string[6, 3] {
+                                { "Low", "Middle", "High"},
+                                { "Low", "Middle", "High"},
+                                { "Middle", "Low", "High"},
+                                { "High", "Low", "Middle"},
+                                { "Middle", "High", "Low"},
+                                { "Low", "High", "Middle"}};
+
+    Dictionary<string, float> stiffnessDict = new Dictionary<string, float>()
+    {
+        {"Low", 0.2f},
+        {"Middle", 0.6f},
+        {"High", 1.0f}
+    };
+
+    Dictionary<string, string> motorDictionary = new Dictionary<string, string>()
+    {
+        {"a", "Down"},
+        {"b", "Left"},
+        {"c", "Up"},
+        {"d", "Right"},
+        {"e", "Front"},
+        {"f", "Back"}
+    };
+
+
+
 
     public GameObject hapticArduinoSerialObj;
     public SerialController hapticArduinoSerialController;
@@ -51,86 +95,25 @@ public class PilotTestControllerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        pilotTest1ControlUI.SetActive(true);
-        pilotTest2ControlUI.SetActive(false);
+        tutorialControlUI.SetActive(false);
+        pilotTest1ControlUI.SetActive(false);
+        pilotTest2ControlUI.SetActive(true);
         currTrialNum = 0;
         currMotorSeqInd = 0;
-        //currTrialSurfaceIndex = 0;
-        //currSurface = trial1Surfaces[0];
-        //makeSurfaceActive(currTrialNum, currTrialSurfaceIndex);
-        //currTrialNumUIText.GetComponent<TMPro.TextMeshProUGUI>().text = "Trial No." + currTrialNum;
-        //currSurfaceUIText.GetComponent<TMPro.TextMeshProUGUI>().text = "Current Surface Name" + currSurface.gameObject.name;
+        currStiffnessInd = 0;
+        groundedSurfaceObj.GetComponent<HapticSurface>().hlStiffness = stiffnessDict[stiffnessSequences[currTrialNum, currStiffnessInd]];
 
         if (!Directory.Exists(Application.persistentDataPath + "/PilotTestData"))
         {
             Directory.CreateDirectory(Application.persistentDataPath + "/PilotTestData");
         }
-        sw = new StreamWriter(Application.persistentDataPath + "/PilotTestData/HapticsPilotTestData_" + System.DateTime.Now.ToString("dd_MMMM_yyyy_HH_mm_ss") + ".csv");
+        sw1 = new StreamWriter(Application.persistentDataPath + "/PilotTestData/HapticsPilotTestData_UserChoices_" + System.DateTime.Now.ToString("dd_MMMM_yyyy_HH_mm_ss") + ".csv");
+        sw2 = new StreamWriter(Application.persistentDataPath + "/PilotTestData/HapticsPilotTestData_HapticParams_" + System.DateTime.Now.ToString("dd_MMMM_yyyy_HH_mm_ss") + ".csv");
+
+        if (HapticDevice == null)
+            HapticDevice = (HapticPlugin)FindObjectOfType(typeof(HapticPlugin));
     }
 
-    /*private void makeSurfaceActive(int _currTrialNum, int _currTrialSurfaceIndex)
-    {
-        if (_currTrialSurfaceIndex < trial1Surfaces.Count)
-        {            
-            switch (_currTrialNum)
-            {
-                case 1:
-                    //setOtherSurfacesInactive();
-                    currSurface = trial1Surfaces[_currTrialSurfaceIndex].gameObject;
-                    trial1Surfaces[_currTrialSurfaceIndex].gameObject.SetActive(true);
-                    break;
-                case 2:
-                    //setOtherSurfacesInactive();
-                    currSurface = trial2Surfaces[_currTrialSurfaceIndex].gameObject;
-                    trial2Surfaces[_currTrialSurfaceIndex].gameObject.SetActive(true);
-                    break;
-                case 3:
-                    //setOtherSurfacesInactive();
-                    currSurface = trial3Surfaces[_currTrialSurfaceIndex].gameObject;
-                    trial3Surfaces[_currTrialSurfaceIndex].gameObject.SetActive(true);
-                    break;
-                case 4:
-                    //setOtherSurfacesInactive();
-                    currSurface = trial4Surfaces[_currTrialSurfaceIndex].gameObject;
-                    trial4Surfaces[_currTrialSurfaceIndex].gameObject.SetActive(true);
-                    break;
-                case 5:
-                    //setOtherSurfacesInactive();
-                    currSurface = trial5Surfaces[_currTrialSurfaceIndex].gameObject;
-                    trial5Surfaces[_currTrialSurfaceIndex].gameObject.SetActive(true);
-                    break;
-            }
-
-            currSurfaceUIText.GetComponent<TMPro.TextMeshProUGUI>().text = "Current Surface Name" + currSurface.gameObject.name;
-        }
-    }
-
-    private void setOtherSurfacesInactive()
-    {
-        //Make all gameobjects in trial1Surfaces, trial2Surfaces, trial3Surfaces, trial4Surfaces, trial5Surfaces inactive
-        foreach (GameObject surface in trial1Surfaces)
-        {
-            surface.gameObject.SetActive(false);
-        }
-        foreach (GameObject surface in trial2Surfaces)
-        {
-            surface.gameObject.SetActive(false);
-        }
-        foreach (GameObject surface in trial3Surfaces)
-        {
-            surface.gameObject.SetActive(false);
-        }
-        foreach (GameObject surface in trial4Surfaces)
-        {
-            surface.gameObject.SetActive(false);
-        }
-        foreach (GameObject surface in trial5Surfaces)
-        {
-            surface.gameObject.SetActive(false);
-        }
-        
-    }*/
-    
     public void startVibration()
     {
         print("Starting vibration for motor " + motorSequences[currTrialNum, currMotorSeqInd]);
@@ -138,14 +121,58 @@ public class PilotTestControllerScript : MonoBehaviour
         StartCoroutine(WaitForSeconds(vibrationDuration, motorSequences[currTrialNum, currMotorSeqInd]));
     }
 
+    public void startVibration(string motorName)
+    {
+        print("Starting vibration for motor " + motorName);
+        hapticTest_startVibration_mid(motorName);
+        StartCoroutine(WaitForSeconds(vibrationDuration, motorName));
+    }
+
     public void onVibrationValChange(float val)
     {
         vibrationSliderValueText.text = "" + val;
+
+        vibrationSliderVal = val;
     }
     public void onStiffnessValChange(float val)
     {
         stiffnessSliderValueText.text = "" + val;
+        stiffnessSliderVal = math.remap(0, 127, 0, 1, val);
+        groundedSurfaceObj.GetComponent<HapticSurface>().hlStiffness = stiffnessSliderVal;
+        //HapticDevice.PhysicsForceDamping = math.remap(0, 127, 0, 1, val);
     }
+    public void onDampingValChange(float val)
+    {
+        dampingSliderValueText.text = "" + val;
+        dampingSliderVal = math.remap(0, 127, 0, 1, val);
+        groundedSurfaceObj.GetComponent<HapticSurface>().hlDamping = dampingSliderVal;
+        //HapticDevice.PhysicsForceStrength = math.remap(0, 127, 0, 1, val);
+    }
+    public void onStaticFrictionValChange(float val)
+    {
+        staticFrictionSliderValueText.text = "" + val;
+        staticFrictionSliderVal = math.remap(0, 127, 0, 1, val);
+        groundedSurfaceObj.GetComponent<HapticSurface>().hlStaticFriction = staticFrictionSliderVal;
+        //HapticDevice.PhysicsForceStrength = math.remap(0, 127, 0, 1, val);
+    }
+    public void onDynamicFrictionChange(float val)
+    {
+        dynamicFrictionSliderValueText.text = "" + val;
+        dynamicFrictionSliderVal = math.remap(0, 127, 0, 1, val);
+        groundedSurfaceObj.GetComponent<HapticSurface>().hlDynamicFriction = dynamicFrictionSliderVal;
+        //HapticDevice.PhysicsForceStrength = math.remap(0, 127, 0, 1, val);
+    }
+
+    /*public void addSelectedHapticValues()
+    {
+        if (sw2 != null)
+        {
+            sw2.WriteLine("Vibration = " + vibrationSliderVal + ", Stiffness = " + stiffnessSliderVal + ", Damping = " + dampingSliderVal + ", StaticFriction = " + staticFrictionSliderVal + ", DynamicFriction = " + dynamicFrictionSliderVal);
+            sw2.Flush();
+            print("Vibration = " + vibrationSliderVal + ", Stiffness = " + stiffnessSliderVal + ", Damping = " + dampingSliderVal + ", StaticFriction = " + staticFrictionSliderVal + ", DynamicFriction = " + dynamicFrictionSliderVal);
+            //print("" +
+        }
+    }*/
 
     IEnumerator WaitForSeconds(float seconds, string motor)
     {
@@ -167,28 +194,30 @@ public class PilotTestControllerScript : MonoBehaviour
     {
         timeOfSelection = System.DateTime.Now.ToString("HH:mm:ss:fff");
     }
-    
+
     public void addSelectedChoice()
     {
         //++currTrialSurfaceIndex;
 
         //makeSurfaceActive(currTrialNum, currTrialSurfaceIndex);
-        if (sw != null)
+        if (sw1 != null)
         {
             var dropDownList = participantChoiceDropDownLists[currMotorSeqInd].GetComponent<TMPro.TMP_Dropdown>();
-            sw.WriteLine("" + currTrialNum + ',' + motorSequences[currTrialNum, currMotorSeqInd] + ',' + dropDownList.options[dropDownList.value].text + ',' + timeVibrationEnd + ',' + timeOfSelection);
-            sw.Flush();
-            print("" + currTrialNum + ',' + motorSequences[currTrialNum, currMotorSeqInd] + ',' + dropDownList.options[dropDownList.value].text + ',' + timeVibrationEnd + ',' + timeOfSelection);
+            sw1.WriteLine("" + currTrialNum + ',' + motorDictionary[motorSequences[currTrialNum, currMotorSeqInd]] + ',' + dropDownList.options[dropDownList.value].text + ',' + timeVibrationEnd + ',' + timeOfSelection);
+            sw1.Flush();
+            print("" + currTrialNum + ',' + motorDictionary[motorSequences[currTrialNum, currMotorSeqInd]] + ',' + dropDownList.options[dropDownList.value].text + ',' + timeVibrationEnd + ',' + timeOfSelection);
             ++currMotorSeqInd;
         }
-        
+
     }
+
 
     private void OnApplicationQuit()
     {
-        sw.Close();
+        sw1.Close();
+        sw2.Close();
     }
-    
+
 
     public void hapticTest_startVibration_max(string _motor)
     {
@@ -197,8 +226,8 @@ public class PilotTestControllerScript : MonoBehaviour
             motor = _motor;
             intensity = "127";
             hapticArduinoSerialController.SendSerialMessage("" + motor + "127");
-            print("" + motor + "127");            
-        }        
+            print("" + motor + "127");
+        }
     }
 
     public void hapticTest_stopVibration(string _motor)
@@ -208,7 +237,7 @@ public class PilotTestControllerScript : MonoBehaviour
             hapticArduinoSerialController.SendSerialMessage("" + motor + "000");
             motor = _motor;
             intensity = "000";
-            print("" + motor + "000");
+            //print("" + motor + "000");
         }
     }
 
@@ -253,7 +282,59 @@ public class PilotTestControllerScript : MonoBehaviour
         {
             pilotTest1ControlUI.SetActive(false);
             pilotTest2ControlUI.SetActive(true);
+            groundedSurfaceObj.SetActive(true);
+            vibrationSurfaceObj.SetActive(true);
+            currTrialNum = 0;
+            sw1.Close();
+            //gotoNextVibrationEqTrial();
         }
     }
-    
+
+    string prevSeq;
+
+    public void recordVibrationData()
+    {
+        if (sw2 != null)
+        {
+            sw2.WriteLine("" + currTrialNum + ',' + prevSeq + ',' + vibrationSliderVal);
+            sw2.Flush();
+            //print("" + currTrialNum + ',' + stiffnessSequences[currTrialNum, currStiffnessInd] + ',' + vibrationSliderVal);
+        }
+        if (currTrialNum == stiffnessSequences.GetLength(0) - 1 && currStiffnessInd == stiffnessSequences.GetLength(1) - 1)
+        {
+            print("Data collection done");
+            pilotTest2ControlUI.SetActive(false);
+            sw2.Close();
+        }
+        ++currStiffnessInd;
+        
+        if (currStiffnessInd < 3)
+        {
+            //print("currTrialNum = " + currTrialNum + ", currStiffnessInd = " + currStiffnessInd);            
+        }
+        else
+        {
+            currStiffnessInd = 0;
+            if (currTrialNum < 6)
+            {
+                ++currTrialNum;
+            }
+            else
+            {
+                //print("Data collection done");
+                //pilotTest2ControlUI.SetActive(false);
+            }
+        }
+
+    }
+
+    public void gotoNextVibrationEqTrial()
+    {
+        print("currTrialNum = " + currTrialNum + ", currStiffnessInd = " + currStiffnessInd);
+        currStiffnessLevelUIText.GetComponent<TMPro.TextMeshProUGUI>().text = stiffnessSequences[currTrialNum, currStiffnessInd];
+        currVibrationTrialNumUIText.GetComponent<TMPro.TextMeshProUGUI>().text = "Trial No." + (currTrialNum + 1);
+        prevSeq = stiffnessSequences[currTrialNum, currStiffnessInd];
+        groundedSurfaceObj.GetComponent<HapticSurface>().hlStiffness = stiffnessDict[stiffnessSequences[currTrialNum, currStiffnessInd]];     
+    }        
+
 }
